ec523c541143348bb7c85a1136ae77bd6644fe11
Madhumitha Viswanathan <madhuvishy@gmail.com>
2014-11-05 11:08:54 -0500

    Valid test for cells in range

--- src/conways/core.cljs
+++ src/conways/core.cljs
@@ -4,20 +4,23 @@
 
 (enable-console-print!)
 
-(def app-state (atom {:width 8 :height 8 :cells []}))
+(def app-state (atom {:dimensions [8 8] :cells []}))
 
 (def offsets [[-1 -1] [0 -1] [1 -1]
               [-1  0]        [1  0]
               [-1  1] [0  1] [1  1]])
 
-(defn alive-neighbors [r c cells]
+(defn valid? [[w h] [x y]]
+  (and (>= x 0) (< x w) (>= y 0) (< y h)))
+
+(defn alive-neighbors [r c {:keys [dimensions cells]}]
   (reduce + (filter 
               identity 
               (map (fn [[offsetx offsety]] 
                      (let [x (+ offsetx r) y (+ offsety c)]
-                       (get-in cells [x y]))) offsets))))
+                       (when (valid? dimensions [x y]) (get-in cells [x y])))) offsets))))
 
-(defn initial-grid [width height]
+(defn initial-grid [[width height]]
   (vec (for [r (range height)] 
          (vec (for [c (range width)] (rand-int 2))))))
 
@@ -31,19 +34,22 @@
                (dom/div #js {:className (if (alive? cell) "alive cell" "dead cell")})))
            (range width))))
 
-(defn world [{:keys [width height cells] :as app} owner]
+(defn world [{:keys [dimensions cells] :as app} owner]
   (reify
-    om/IDidMount 
-    (did-mount [_]
-      (om/transact! app :cells (fn [_] (initial-grid width height))))
+    om/IWillMount 
+    (will-mount [_]
+      (om/transact! app :cells (fn [_] (initial-grid dimensions))))
+
+    om/IDidUpdate
+    (did-update [_ _ _]
+      (prn (alive-neighbors 7 7 app)))
 
     om/IRender
     (render [_]
-      (when (not (empty? cells))
-        (prn (alive-neighbors 4 4 cells)))
-      (dom/div #js {:ref "cells" :id "cells"}
-               (apply dom/div nil
-                      (map (fn [r] (row width cells r)) (range height)))))))
+      (let [[width height] dimensions]
+        (dom/div #js {:ref "cells" :id "cells"}
+                 (apply dom/div nil
+                        (map (fn [r] (row width cells r)) (range height))))))))
 
 (om/root
   (fn [app owner]
